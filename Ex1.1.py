db3eba4ffc8fc090c9c3bdf3f757e220a8ace857
unknown <Davealvarez91@hotmail.com>
2023-09-22 20:17:18 -0600

    prueba git

---
+++ Ex1.1.py
@@ -0,0 +1,7 @@
+#Solicitar la edad a un usuario y mostrarle en pantalla cuanto tendrá dentro de 5 años
+#Se solicita la edad del usuario
+user_age=int(input("Favor ingresar su edad "))
+#Se crea una variable donde se hace la operacion para obtener el resultado de la edad dentro de 5 años
+age_in_5_years=(user_age+5)
+#Se imprime la respuesta de la edad que tendrá dentro de 5 años
+print("Su edad dentro de 5 años será:",age_in_5_years,"años")
